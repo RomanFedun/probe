539708db73aeadfe1595325b157a703480834c88
Roman <fero5uin@gmail.com>
2019-04-16 23:13:22 +0300

    my first udemy task

---
+++ taskUdemy_1/index1.js
@@ -0,0 +1,16 @@
+let money = prompt("budget per month");
+let time = prompt("input date YYYY-MM-DD");
+let quest1 = prompt("enter the required articie of expenditure in this month");
+let quest2 = prompt("how much it will cost"); 
+let appData = {
+budget: money,
+timeData: time,
+expenses: { 
+[quest1]: quest2
+},
+optionalExpenses: {},
+income: [],
+savings: false
+}
+console.log(appData);
+alert(money/30);
