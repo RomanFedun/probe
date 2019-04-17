6631e79fccf32c77ef305bd8e52eaf74d8f0900a
Roman <fero5uin@gmail.com>
2019-04-17 22:53:36 +0300

    continue task_1 udemy

--- taskUdemy_1/index1.js
+++ taskUdemy_1/index1.js
@@ -1,16 +1,22 @@
-let money = prompt("budget per month");
+let money = +prompt("budget per month");
 let time = prompt("input date YYYY-MM-DD");
-let quest1 = prompt("enter the required articie of expenditure in this month");
-let quest2 = prompt("how much it will cost"); 
+//let quest1 = prompt("enter the required articie of expenditure in this month");
+//let quest2 = prompt("how much it will cost"); 
 let appData = {
-budget: money,
-timeData: time,
-expenses: { 
-[quest1]: quest2
-},
-optionalExpenses: {},
-income: [],
-savings: false
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
 }
+
+for (var i = 0; i < 2; i++) {
+    var quest1 = prompt("enter the required articie of expenditure in this month"),
+        quest2 = +prompt("how much it will cost");
+    appData.expenses[quest1] = quest2;
+    }
+
+appData.moneyPerDay = appData.budget / 30;
 console.log(appData);
-alert('budget per one day: ' + money/30);
+alert('budget per one day: ' + appData.moneyPerDay);
